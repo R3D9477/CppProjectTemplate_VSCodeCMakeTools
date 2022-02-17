415e2c7dda271f38b62244ac6a3c771a43dfed2d
r3d9477 <r3d9477@gmail.com>
2022-02-17 15:57:43 +0300

    fix for uint testing autostart on build

--- test/test_UCMakeSkeleton.cpp
+++ test/test_UCMakeSkeleton.cpp
@@ -1,12 +1,12 @@
 
 // ------------------------------------------------------------------------------------------
 
-#define BOOST_TEST_MODULE test_somelib
+#define BOOST_TEST_MODULE test_UCMakeSkeleton
 
 #include <boost/test/unit_test.hpp>
-BOOST_AUTO_TEST_SUITE(test_somelib)
+BOOST_AUTO_TEST_SUITE(test_UCMakeSkeleton)
 
-BOOST_AUTO_TEST_CASE(test_somelib_case)
+BOOST_AUTO_TEST_CASE(test_UCMakeSkeleton_case)
 {
     //...
     //...
