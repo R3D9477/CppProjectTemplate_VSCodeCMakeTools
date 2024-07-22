7ce44e5876e3c57b31465b556d4f1901b7cae1ae
Evgeni Kruglikov <evgeni.kruglikov@partner.bmw.de>
2024-07-22 11:41:48 +0200

    tasks

--- main.cpp
+++ main.cpp
@@ -13,12 +13,13 @@ int main()
     std::cout << "C++ compiler version: " << cppInfo.GetCompilerVersion()
               << std::endl;
 
-    while (true)
+    constexpr std::size_t maxElements = 1000000U;
+    for (std::size_t s = 0U; s < maxElements; ++s)
     {
         std::cout << 1;
 
-        // int *p = new int[1];
-        //*p = 1;
+        int *p = new int[1];
+        *p = 1;
     }
 
     return 0;
