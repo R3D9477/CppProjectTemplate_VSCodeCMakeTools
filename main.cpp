de10712007b000af1a1102f1f72bf8763140b63f
Evgeni Kruglikov <evgeni.kruglikov@partner.bmw.de>
2024-07-22 14:27:24 +0200

    upd.main.example

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "cppinfo.hpp"
 #include "somelib.hpp"
 
+#include <cstdint>
 #include <iostream>
 
 int main()
 {
     std::cout << "Hi." << std::endl;
+
+#pragma region Region_CPP_INFO
 
     CppInfo cppInfo;
     std::cout << "C++ language version: " << cppInfo.GetLanguageVersion()
@@ -13,13 +16,19 @@ int main()
     std::cout << "C++ compiler version: " << cppInfo.GetCompilerVersion()
               << std::endl;
 
-    for (std::size_t s = 0U; s < 100000000; ++s)
-    {
-        std::cout << s;
+#pragma endregion Region_CPP_INFO
 
-        int *p = new int[1];
-        *p = 100;
+#pragma region Region_PROBLEM_TESTING
+/*
+    std::cout << "Problematic code for testing:" << std::endl;
+    for (std::size_t s = 0U; s < 5000000; ++s)
+    {
+        std::uint8_t *p = new std::uint8_t[1];
+        *p = 0xFF;
+        std::cout << "    Allocated: " << s << " byte(s)" << std::endl;
     }
+*/
+#pragma endregion Region_PROBLEM_TESTING
 
     return 0;
 }
