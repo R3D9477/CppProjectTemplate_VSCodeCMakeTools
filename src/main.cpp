53df0554919224d31a0d5da123cd623623929d5d
R3D9477 <r3d9477@gmail.com>
2024-11-21 12:00:28 +0100

    Qt6example

--- src/main.cpp
+++ src/main.cpp
@@ -12,3 +12,27 @@ int main(int argc, char *argv[])
 
     return app.exec();
 }
+
+/*
+
+#include <QApplication>
+#include <QPushButton>
+#include <QQuickWidget>
+#include <QUrl>
+
+#include <QtPlugin>
+
+Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+Q_IMPORT_PLUGIN(QtQuick2Plugin);
+
+int main(int argc, char** argv) {
+    QApplication a(argc, argv);
+
+    QQuickWidget* quick = new QQuickWidget();
+    quick->setSource(QUrl::fromLocalFile("wp.qml"));
+    quick->show();
+
+    return a.exec();
+}
+
+*/
